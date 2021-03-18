7e3844f63942c560eab5e8a320a2d4f76e546ece
Kalmovic <joseph.kaltenecker@gmail.com>
2021-03-18 08:31:52 -0300

    fix styled component importation

--- src/NewTransactionModal/index.tsx
+++ src/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
-import { Container } from '../components/TransactionsTable/styles';
+import { Container } from './styles';
 
 // import { Container } from './styles';
 
