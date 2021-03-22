dd8ac1158270a61b6b97d87c64d73f8377b7c303
Kalmovic <joseph.kaltenecker@gmail.com>
2021-03-22 17:21:27 -0300

    add calculator for total

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,9 +6,24 @@ import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from './styles';
 
 const Summary: React.FC = () => {
-  const transactions = useContext(TransactionsContext);
+  const {transactions} = useContext(TransactionsContext);
 
-  console.log(transactions);
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income' ) {
+      acc.income += transaction.value;
+      acc.total += transaction.value;
+    } else {
+      if (transaction.type === 'outcome' ) {
+        acc.outcome += transaction.value; 
+        acc.total -= transaction.value; 
+      }
+    }
+    return acc;
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0
+  });
 
   return (
     <Container>
@@ -17,21 +32,30 @@ const Summary: React.FC = () => {
           <p>Income</p>
           <img src={incomeImage} alt="income section" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(summary.income)}</strong>
       </div>
       <div>
         <header>
           <p>Outcome</p>
           <img src={outcomeImage} alt="income section" />
         </header>
-        <strong>- R$100,00</strong>
+        <strong>{new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(summary.outcome)}</strong>
       </div>
       <div className="total">
         <header>
           <p>Total</p>
           <img src={totalImage} alt="income section" />
         </header>
-        <strong>R$900,00</strong>
+        <strong>{new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(summary.total)}</strong>
       </div>
     </Container>
   );
---
+++ src/utils/formatNumber.ts
@@ -0,0 +1,10 @@
+interface TransactionValue {
+    value: number;
+  }
+
+export function formatNumber(value: TransactionValue) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(Number(value))
+}
